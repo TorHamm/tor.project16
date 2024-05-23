462ddc8dd9043a60eaf68a13794cf243d705a349
TorHamm <tor.newacc@gmail.com>
2024-05-23 11:55:14 +0700

    initial files

---
+++ scripts/app.js
@@ -0,0 +1,27 @@
+const button = document.querySelectorAll('button');
+const submitButton = document.getElementById('submitButton');
+const ratingCard = document.getElementById('ratingCard');
+const thankYouCard = document.getElementById('thankYouCard');
+const ratingText = document.getElementById('ratingText');
+
+let rating = null;
+
+const handleRate = (rate) => {
+    rating = rate;
+}
+
+const handleSubmit = () => {
+    ratingCard.classList.add('hidden');
+    thankYouCard.classList.remove('hidden');
+    ratingText.innerHTML = "You selected " + rating + " out of 5"
+}
+
+button.forEach(selectedBtn => {
+    selectedBtn.addEventListener('click' , () => {
+        button.forEach(unSelectedBtn => {
+            unSelectedBtn.classList.remove('active');
+        })
+        selectedBtn.classList.add('active');
+        submitButton.style.pointerEvents = "auto";
+    });
+});
